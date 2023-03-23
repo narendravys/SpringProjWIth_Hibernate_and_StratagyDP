40cb49b32bdf4626d2c016adcf47a4b064283f8f
TL <Narendra@DESKTOP-RMDE9Q0>
2023-03-23 23:46:23 +0530

    Project Created Initially

---
+++ SpringProj1_withHibernate/src/main/java/com/nv/SpringProj1_withHibernate/App.java
@@ -0,0 +1,13 @@
+package com.nv.SpringProj1_withHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
